55317453c06112dee2732cf045e50bb2491a77b0
Sameer Ahire <sameerahire6399@gmail.com>
2024-04-16 11:49:49 -0400

    initial commit

--- App.js
+++ App.js
@@ -5,11 +5,15 @@ import cors from "cors";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
 //const express = require('express')
 
+mongoose.connect("mongodb+srv://Cluster42453:YFpCe3NrXXNu@cluster42453.swjdzbr.mongodb.net");
 const app = express()
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 Hello(app)
 ModuleRoutes(app);
 AssignmentRoutes(app);
